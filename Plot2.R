305f637ba46a9db4ddb4fcc6e501d9c306929236
LouisConserve <louisconserve@gmail.com>
2016-05-02 21:42:02 -0400

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -1,8 +1,12 @@
+#Open file
 openfile <- "./household_power_consumption.txt"
+#Read data
 data <- read.table(openfile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+#Correct selection
 selection <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 datetime <- strptime(paste(selection$Date, selection$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 globalActivePower <- as.numeric(subSetData$Global_active_power)
+#Formattings
 png("plot2.png", width=480, height=480)
 plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
